4e8a1622195dd986edaa117a566e6aa9e09d8d31
Zlatin Ivanov <>
2018-03-31 16:42:12 +0300

    Auth part 1: Build three components, Public, Private, and Login

--- src/App.js
+++ src/App.js
@@ -1,63 +1,41 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import './App.css';
+import {
+  Route,
+  Link,
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
-const Courses = ({ match }) => (
-  <div>
-     <ul>
-        <li><Link to={`${match.url}/technology`}>Technology</Link></li>
-        <li><Link to={`${match.url}/business`}>Business</Link></li>
-        <li><Link to={`${match.url}/economics`}>Economics</Link></li>
-    </ul>
-
-    <Route exact path={`${match.path}/:course`} render={({match}) => (<div> This is {match.params.course} </div>)}/>
-  </div>
+const Public = () => (
+  <div> This is a public page </div>
 );
 
-
-const Home = () => (
-  <div>
-    <h2> Home </h2>
-  </div>
+const Private = () => (
+  <div> This is a private page </div>
 );
 
-const Airport = () => (
-  <div>
-     <ul>
-      <li>Jomo Kenyatta</li>
-      <li>Tambo</li>
-      <li>Murtala Mohammed</li>
-    </ul>
-  </div>
+const Login = () => (
+  <div> Login Page <button>login</button> </div>
 );
 
-const City = () => (
-  <div>
-    <ul>
-      <li>San Francisco</li>
-      <li>Istanbul</li>
-      <li>Tokyo</li>
-    </ul>
-  </div>
-);
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/courses">Courses</Link></li>
-          <li><Link to="/cities">Cities</Link></li>
-        </ul>
+      <Router>
+        <div style={{width: 1000, margin: '0 auto'}}>
+          <ul>
+            <li><Link to='/public'> Public </Link></li>
+            <li><Link to='/private'> Private </Link></li>
+          </ul>
 
-        <Route path="/" exact component={Home}/>
-        <Route path="/courses" component={Courses}/>
-        <Route path="/cities" component={City}/>
-      </div>
+          <hr/>
+
+          <Route path='/public' component={Public} />
+          <Route path='/private' component={Private} />
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
