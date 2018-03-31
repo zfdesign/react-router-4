ed76d083874bccc104fbc359aea81af31976e1b8
Zlatin Ivanov <>
2018-03-31 16:03:07 +0300

    Added Home, Airpot and City components. Incl. routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+
+
+const Home = () => (
+  <div>
+    <h2> Home </h2>
+  </div>
+);
+
+const Airport = () => (
+  <div>
+     <ul>
+      <li>Jomo Kenyatta</li>
+      <li>Tambo</li>
+      <li>Murtala Mohammed</li>
+    </ul>
+  </div>
+);
+
+const City = () => (
+  <div>
+    <ul>
+      <li>San Francisco</li>
+      <li>Istanbul</li>
+      <li>Tokyo</li>
+    </ul>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/airports">Airports</Link></li>
+          <li><Link to="/cities">Cities</Link></li>
+        </ul>
+
+        <Route path="/" component={Home}/>
+        <Route path="/airports" component={Airport}/>
+        <Route path="/cities" component={City}/>
       </div>
     );
   }
