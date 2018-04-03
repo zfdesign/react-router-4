0846a2115dd1556ad4c6de13220b91eff9b88bc6
zfdesign <zlatinivanov01@gmail.com>
2018-04-03 11:19:41 +0300

    Customising links: Use of ‘Route children’  with ‘match’
    This render prop takes in a function that receives all the same route props
    as the component and render methods, except when a route doesn't match
    the URL location.

--- src/App.js
+++ src/App.js
@@ -1,102 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react'
 import {
-  Route,
-  Link,
-  Redirect,
   BrowserRouter as Router,
-} from 'react-router-dom';
-import { withRouter } from 'react-router'
+  Route,
+  Link
+} from 'react-router-dom'
 
-const Public = () => (
-  <div> This is a public page </div>
-);
 
-const Private = () => (
-  <div> This is a private page </div>
-);
+const Home = () => (
+  <div>
+    <h2>Home Page</h2>
+  </div>
+)
 
-class Login extends React.Component {
-  state = {
-    redirectToPreviousRoute: false
-  };
+const Contact = () => (
+  <div>
+    <h2>Contact Page</h2>
+  </div>
+)
 
-  login = () => {
-    AuthService.authenticate(() => {
-      this.setState({ redirectToPreviousRoute: true });
-    });
-  };
+const CustomLink = ({ children, to, exact }) => (
+  <Route path={to} exact={exact} children={({ match }) => (
+    <div className={match ? 'active' : ''}>
+      {match ? '> ' : ''}
+      <Link to={to}>
+        {children}
+      </Link>
+    </div>
+  )}/>
+)
 
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: "/" } };
-    const { redirectToPreviousRoute } = this.state;
-
-    if (redirectToPreviousRoute) {
-      return <Redirect to={from} />;
-    }
-
-    return (
-      <div>
-        <p>You must log in to view the page at {from.pathname}</p>
-        <button onClick={this.login}>Log in</button>
-      </div>
-    );
-  }
-}
-
-const AuthService = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100)
-  },
-  logout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100)
-  }
-};
-
-const SecretRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    AuthService.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to={{
-          pathname: '/login',
-          state: { from: props.location }
-        }} />
-  )} />
-);
-
-const AuthStatus = withRouter(({ history }) => (
-  AuthService.isAuthenticated ? (
-    <p>
-      Welcome! <button onClick={() => {
-        AuthService.logout(() => history.push('/'))
-      }}>Sign out</button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  )
-));
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <Router>
-        <div style={{width: 1000, margin: '0 auto'}}>
-          <AuthStatus />
-          <ul>
-            <li><Link to='/public'> Public </Link></li>
-            <li><Link to='/private'> Private </Link></li>
-          </ul>
+        <div>
+            <CustomLink exact={true} to="/">
+              Home
+            </CustomLink>
+            <CustomLink to="/contact">
+              Contact
+            </CustomLink>
 
           <hr/>
 
-          <Route path='/public' component={Public} />
-          <Route path="/login" component={Login}/>
-          <SecretRoute path='/private' component={Private} />
+          <Route exact path="/" component={Home}/>
+          <Route path="/contact" component={Contact}/>
         </div>
       </Router>
-    );
+    )
   }
 }
 
